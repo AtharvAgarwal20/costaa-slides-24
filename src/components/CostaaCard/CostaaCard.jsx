75b05e9e26434d099c5c11cf2f738d123f487c1f
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-04-03 05:21:53 +0530

    fix: fixed layout

--- src/components/CostaaCard/CostaaCard.jsx
+++ src/components/CostaaCard/CostaaCard.jsx
@@ -1,42 +1,18 @@
 import * as styles from './CostaaCard.module.scss'
 
 import { useRef } from 'react'
-import { useGlitch } from 'react-powerglitch'
+
+import cardFrame from '../../assets/cardFrame.png'
 
 export default function CostaaCard({ details }) {
-    const glitch = useGlitch({
-        playMode: "always",
-        createContainers: true,
-        hideOverflow: false,
-        timing: {
-            delay: 2000,
-            duration: 9000,
-            easing: "ease-in-out",
-        },
-        glitchTimeSpan: {
-            start: 0.1,
-            end: 0.2,
-        },
-        shake: {
-            velocity: 15,
-            amplitudeX: 0.01,
-            amplitudeY: 0.01,
-        },
-        slice: {
-            count: 6,
-            velocity: 15,
-            minHeight: 0.01,
-            maxHeight: 0.05,
-            hueRotate: true,
-        },
-        pulse: false,
-    });
-
     return (
         <div
             className={styles.card}
-            ref={glitch.ref}
         >
+            <img
+                src={cardFrame}
+                alt="card frame"
+            />
             <img
                 src={details.img}
                 alt="costaan"
